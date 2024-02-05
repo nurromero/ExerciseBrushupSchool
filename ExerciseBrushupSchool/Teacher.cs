7c2b314cc9c6cca9cd233d53cd76795b8f4e8f7c
Nuriye <nuriyeerdogan38@gmail.com>
2024-02-05 22:21:22 +0100

    Add project files.

---
+++ ExerciseBrushupSchool/Teacher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseBrushupSchool
+{
+    public class Teacher
+    {
+
+        // Properties
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Salary: {Salary}";
+        }
+
+        // First method
+        public void ValidateName()
+        {
+            if (Name == null)  
+                throw new ArgumentNullException("Name is null!");
+
+            if (Name.Length < 1)
+                throw new ArgumentException("Name must be ATLEAST one character!");
+        }
+
+        // Second method :D
+        public void ValidateSalary()
+        {
+            if (Salary < 0)
+                throw new ArgumentOutOfRangeException("Salary must be over 0.");
+
+        }
+
+        // Single general validator method, calling two property based validator methods
+        public void Validate()
+        {
+            ValidateName();
+            ValidateSalary();
+        }
+
+
+    }
+
+}
---
+++ ExerciseBrushupSchoolTests/TeacherTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExerciseBrushupSchool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseBrushupSchool.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            //Arrange
+            var teacher = new Teacher { Id = 1, Name = "Nur Nur", Salary = 50000 };
+            var expectedString = "ID: 1, Name: Nur Nur, Salary: 50000";
+
+            //Act
+            var result = teacher.ToString();
+
+            //Assert
+            Assert.AreEqual(expectedString, result);
+         
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            //Arrange
+            var teacher = new Teacher { Name = "Z" };
+
+
+            // Act and Assert
+            teacher.ValidateName();
+
+
+        }
+
+        [TestMethod()]
+        public void ValidateSalaryTest()
+        {
+            //Arrange 
+            var teacher = new Teacher { Salary = -1 };
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => teacher.ValidateSalary());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
